d79c3b78afc319ada62f8de7312a8b7bdfbb2079
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-03 22:53:13 +0100

    Update WeatherBox and WeatherSummary components

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,18 +1,28 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 
 const WeatherBox = props => {
+  const [weatherData, setWeatherData] = useState(null);
 
   const handleCityChange = useCallback((city) => {
-    console.log(city);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5d9df579968077defb121d007ddd656a&units=metric`)
+      .then(res => res.json())
+      .then(data => {
+        setWeatherData({
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main
+        });
+      });
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
+      <WeatherSummary {...weatherData} />
       <Loader />
     </section>
   )
